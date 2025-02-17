f3e798b6ae8387581a90bf5c62be2cc9e6073a9a
Rimo P.M <rimoputramaisal12@gmail.com>
2025-02-17 11:01:50 +0700

    login+halaman utama

---
+++ UKK2025_XIIPPLG2_7915/index.php
@@ -0,0 +1,101 @@
+<style>
+    .card {
+        transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+    }
+
+    .card:hover {
+        transform: translateY(-5px);
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+    }
+
+    .navbar {
+        background: linear-gradient(135deg, rgb(66, 66, 66), rgb(139, 17, 17));
+    }
+
+    .sb-sidenav {
+        background: linear-gradient(135deg, rgb(60, 61, 61), rgb(139, 17, 17));
+    }
+
+    .sb-sidenav a {
+        color: white !important;
+    }
+
+    .sb-sidenav-footer {
+        background: rgb(124, 11, 11) !important;
+        color: white;
+    }
+
+    .breadcrumb {
+        background: rgb(61, 61, 61);
+        border-radius: 5px;
+        padding: 10px;
+    }
+</style>
+
+<?php
+include "koneksi.php";
+if (!isset($_SESSION['users'])) {
+    header('location:login.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Web Kasir</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark ">
+        <a class="navbar-brand ps-3 text-white" href="index.php">To do List</a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark bg-dark " id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading ">Fitur</div>
+                        <a class="nav-link " href="index.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Dashboard
+                        </a>
+                        <a class="nav-link " href="?page=tugas">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            tugas
+                        </a>
+
+                        <a class="nav-link" href="logout.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                            Logout
+                        </a>
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer ">
+                    <div class="small">Logged in as:</div>
+
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+
+            </main>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+            <script src="js/scripts.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+            <script src="assets/demo/chart-area-demo.js"></script>
+            <script src="assets/demo/chart-bar-demo.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+            <script src="js/datatables-simple-demo.js"></script>
+</body>
+
+</html>
---
+++ UKK2025_XIIPPLG2_7915/registrasi.php
@@ -0,0 +1,98 @@
+<?php
+include "koneksi.php";
+if (isset($_POST['username'])) {
+
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    $name = 'rimo';
+
+    $insert = mysqli_query($koneksi, "INSERT INTO users(username,password,email,name) VALUES('$username','$password','$email','$name')");
+
+    if ($insert) {
+        echo '<script>alert("Registrasi Berhasil!"); location.href="login.php"</script>';
+    } else {
+        echo '<script>alert("Register gagal.");</script>';
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Web To do List</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background: linear-gradient(135deg, rgb(172, 203, 17) 0%, #2575fc 100%);
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #fff;
+        }
+
+        .login-container {
+            background-color: rgba(255, 255, 255, 0.9);
+            border-radius: 15px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            padding: 30px;
+            width: 100%;
+            max-width: 400px;
+        }
+
+        .form-control:focus {
+            border-color: rgb(135, 203, 17);
+            box-shadow: 0 0 0 0.2rem rgba(106, 17, 203, 0.25);
+        }
+
+        .btn-custom {
+            background: linear-gradient(135deg, rgb(17, 150, 203), #2575fc);
+            color: #fff;
+            border: none;
+        }
+
+        .btn-custom:hover {
+            background: linear-gradient(135deg, #2575fc, rgb(17, 132, 203));
+        }
+
+        .logo {
+            width: 80px;
+            height: 80px;
+            margin: 0 auto 20px;
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="login-container text-center">
+        <img src="images.png" alt="Logo" class="logo rounded-circle">
+        <h3 class="mb-4 text-primary">Registrasi Web To do List</h3>
+        <form method="post">
+            
+            <div class="mb-3">
+                <input class="form-control" id="inputEmail" type="text" name="username" placeholder="Masukkan Username" />
+            </div>
+            <div class="mb-3">
+                <input class="form-control" id="inputPassword" type="password" name="password" placeholder="Masukkan Password" />
+            </div>
+            <div class="mb-3">
+                <input class="form-control" id="inputEmail" type="text" name="name" placeholder="Masukkan Nama" />
+            </div>
+            <div class="mb-3">
+                <input class="form-control" id="inputEmail" type="text" name="email" placeholder="Masukkan Email" />
+            </div>
+            <button type="submit" class="btn btn-custom w-100 mb-3">Registrasi</button>
+        </form>
+        <p class="mt-3 mb-0 text-dark">sudah punya Akun? <a href="login.php" class="text-decoration-none">Login</p>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
+<form>
